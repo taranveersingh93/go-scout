810525fc76f33f3b38f1c66a4bb73cea8d52183e
Taranveer Singh <taranveersingh93@gmail.com>
2023-10-19 15:20:38 -0400

    Add a basic structure

--- js/site.js
+++ js/site.js
@@ -1,10 +1,84 @@
-const displayMessage = () => {
-    const msg = document.getElementById("message").value;
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
-        }
-    );
+const events = [
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 240000,
+        date: "06/01/2017",
+    },
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 250000,
+        date: "06/01/2018",
+    },
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 257000,
+        date: "06/01/2019",
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 130000,
+        date: "06/01/2017",
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 140000,
+        date: "06/01/2018",
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 150000,
+        date: "06/01/2019",
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 40000,
+        date: "06/01/2017",
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 45000,
+        date: "06/01/2018",
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 50000,
+        date: "06/01/2019",
+    },
+];
+
+const getEvents = () => {
+    return events;
 }
+
+const buildDropDown = () => {
+    const currentEvents = getEvents();
+    const cityNames = currentEvents.map(event => event.city);
+    const uniqueCities = new Set(cityNames);
+    const dropDownChoices = ['All', ...uniqueCities];
+    const dropdownTemplate = document.querySelector('#dropdownItemTemplate');
+    const cityDropdown = document.querySelector('#cityDropdown');
+    for (let i = 0; i < dropDownChoices.length; i++) {
+        const dropdownItem = dropdownTemplate.content.cloneNode(true);
+        const cityName = dropDownChoices[i];
+        dropdownItem.querySelector('a').innerText = cityName;
+        cityDropdown.appendChild(dropdownItem);
+    }
+}    
